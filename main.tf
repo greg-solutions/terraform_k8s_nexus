1aa8924d7ef694a136a9d8f2f9881d1468e6a765
Serhii Trykoza <serhii.trykoza@gregsolutions.dev>
2020-03-18 19:47:42 +0200

    WIP

--- main.tf
+++ main.tf
@@ -1,70 +1,16 @@
-resource "kubernetes_deployment" "nexus" {
-  metadata {
-    name = "terraform-nexus"
-    labels = {
-      test = "MyExampleNexus"
+module "deploy_redis" {
+  source = "git::https://bitbucket.gregsolutions.dev/scm/gsiaac/terraform_k8s_deploy?ref=v1.0.0"
+  region = var.region
+  name = var.name
+  namespace = var.namespace
+  image = var.docker_image
+  internal_port = var.ports_mapping
+  volume_mount = var.volume_mount
+  volume_efs = [
+    {
+      path_on_efs = var.efs_volume_path
+      efs_id = var.efs_id
+      volume_name = "nexus-config"
     }
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        test = "MyExampleNexus"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          test = "MyExampleNexus"
-        }
-      }
-
-      spec {
-        container {
-          image = "sonatype/nexus3:3.21.1"
-          name  = "nexus"
-          #volume {
-          #  name = "var_efs_volume_path"
-          #}
-          /*
-          nfs {
-            path      =
-            read_only =
-            server    =
-          }
-          */
-        }
-        /*
-                  resources {
-                    limits {
-                      cpu    = "0.5"
-                      memory = "512Mi"
-                    }
-                    requests {
-                      cpu    = "250m"
-                      memory = "50Mi"
-                    }
-                  }
-
-                  liveness_probe {
-                    http_get {
-                      path = "/nginx_status"
-                      port = 80
-
-                      http_header {
-                        name  = "X-Custom-Header"
-                        value = "Awesome"
-                      }
-                    }
-
-                    initial_delay_seconds = 3
-                    period_seconds        = 3
-                  }
-        */
-      }
-    }
-  }
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,31 @@
+variable "efs_volume_path" {}
+variable "namespace" {}
+variable "region" {}
+variable "efs_id" {}
+
 variable "name" {
   default = "nexus"
 }
-/*
-variable "efs_volume_path" {
-  default = "/rancher-volume/nexus-config"
+
+variable "volume_mount" {
+  default = [
+    {
+      mount_path = "/data"
+      sub_path = "nexus-config"
+      volume_name = "nesus-config"
+    }
+  ]
 }
-*/
+
+variable "ports_mapping" {
+  default = [
+    {
+      name = "general-port"
+      internal_port = "8081"
+      external_port = "8081"
+    }
+  ]
+}
+variable "docker_image" {
+  default = "sonatype/nexus3:3.21.1"
+}
